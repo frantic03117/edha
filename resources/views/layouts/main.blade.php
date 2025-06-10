08ddb660d7b932a8235cbe522b9552512adfdae8
frantic03117 <shahrukh@franticpro.com>
2025-06-10 11:53:19 +0530

    facebook add

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,32 @@
     <link rel="manifest" href="{{ url('public/favicons') }}/site.webmanifest">
     <meta content="Umesh Upadhayay" name="author">
     <!-- App favicon -->
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1887710252071110');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1887710252071110&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
     <link rel="shortcut icon" href="favicons/images-favicon.ico">
 
     {{-- <script src="{{ url('public/js/jquery-jquery.min.js') }}"></script> --}}
